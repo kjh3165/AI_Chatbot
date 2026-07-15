fd82a84af854169810ef8b46418273a7b358b5af
kjh3165 <jhwk109@gmail.com>
2026-07-15 11:32:50 +0900

    챗봇 설정 config 파일로 메모리 설정 추가

--- src/main/java/com/ai_chatbot/chatAi/ApiV1ChatController.java
+++ src/main/java/com/ai_chatbot/chatAi/ApiV1ChatController.java
@@ -2,6 +2,7 @@ package com.ai_chatbot.chatAi;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,9 +17,13 @@ public class ApiV1ChatController {
     private final ChatClient chatClient;
 
     @GetMapping("/ai")
-    public Map<String, String> chat(@RequestParam String message) {
+    public Map<String, String> chat(@RequestParam String message,
+                                    @RequestParam(defaultValue = "default") String conversationId // 추가 - conversationId 기본 값 default 설정
+    ) {
         String response = chatClient.prompt()
                 .user(message)
+                // 추가 - 대화방 ID, 이 값으로 이전 대화를 꺼내오고 응답도 저장함
+                .advisors(a -> a.param(ChatMemory.CONVERSATION_ID, conversationId))
                 .call()
                 .content();
 
